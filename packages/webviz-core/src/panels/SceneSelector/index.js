2450c87a17996466703891a6ab05bb1938284702
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-12-15 12:07:59 +0900

    Remove header comment

--- packages/webviz-core/src/panels/SceneSelector/index.js
+++ packages/webviz-core/src/panels/SceneSelector/index.js
@@ -1,11 +1,5 @@
 // @flow
-//
-//  Copyright (c) 2018-present, Cruise LLC
-//
-//  This source code is licensed under the Apache License, Version 2.0,
-//  found in the LICENSE file in the root directory of this source tree.
-//  You may not use this file except in compliance with the License.
-//
+// eslint-disable-next-line header/header
 import * as React from "react";
 import { hot } from "react-hot-loader/root";
 import { useRosLib, SceneSelector } from "scene-viewer-panels";
